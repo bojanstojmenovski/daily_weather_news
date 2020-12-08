dde27008c418a5b43c9f9552572720ea4825d096
Bojan Stojmenovski <bojan@Bojans-MacBook-Air.local>
2020-12-08 18:25:22 +0100

    Updates

---
+++ precache-manifest.a8ab702fabc3d6dc57aefaa7b11841f7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e20778dbba71aff4c743324dc501e30",
+    "url": "/daily_weather_news/index.html"
+  },
+  {
+    "revision": "450585914d2d839da7de",
+    "url": "/daily_weather_news/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "510413a4afb57d351960",
+    "url": "/daily_weather_news/static/css/main.eafd973f.chunk.css"
+  },
+  {
+    "revision": "450585914d2d839da7de",
+    "url": "/daily_weather_news/static/js/2.de80a6db.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/daily_weather_news/static/js/2.de80a6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510413a4afb57d351960",
+    "url": "/daily_weather_news/static/js/main.7f28ccad.chunk.js"
+  },
+  {
+    "revision": "8cc77fb9d9bc81da0f17",
+    "url": "/daily_weather_news/static/js/runtime-main.626934b1.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/daily_weather_news/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/daily_weather_news/static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  },
+  {
+    "revision": "e56667f374850924ef536935db5d7291",
+    "url": "/daily_weather_news/static/media/cloud.e56667f3.svg"
+  },
+  {
+    "revision": "1739930d7c3475d0a38a3ea801a2f40e",
+    "url": "/daily_weather_news/static/media/few_clouds.1739930d.svg"
+  },
+  {
+    "revision": "f1e80e805cdaaac7e1593874b7504efe",
+    "url": "/daily_weather_news/static/media/logo.f1e80e80.svg"
+  },
+  {
+    "revision": "fa1531c36ae0dfa0d681ad4b6c507cbb",
+    "url": "/daily_weather_news/static/media/rain.fa1531c3.svg"
+  },
+  {
+    "revision": "ade49d8d4ca18f2fd7dc64dc0c9e72ae",
+    "url": "/daily_weather_news/static/media/rainy_clouds.ade49d8d.jpg"
+  },
+  {
+    "revision": "a310e43e9e938cb95fbb5a8256dad02e",
+    "url": "/daily_weather_news/static/media/search_white.a310e43e.png"
+  },
+  {
+    "revision": "e1bb2cc73021b3967272fca53a84621f",
+    "url": "/daily_weather_news/static/media/sun.e1bb2cc7.svg"
+  }
+]);
